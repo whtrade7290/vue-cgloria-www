c467643f2281911de29f85654f2fd34dded11a72
정우현 <whtrade7290@gmail.com>
2025-10-10 14:24:11 +0900

    test

--- src/api/intercept.js
+++ src/api/intercept.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getUserIdFromCookie } from '@/utils/cookie.ts'
 import store from '@/store/index'
 import Swal from 'sweetalert2'
-
+// test
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   timeout: 2000
