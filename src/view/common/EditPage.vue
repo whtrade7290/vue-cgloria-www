522667c09f1b1f96b30743dec36e40e68a4d5c2a
정우현 <whtrade7290@gmail.com>
2026-07-05 17:53:16 +0900

    fix: 글수정 -> 글등록으로 변경

--- src/view/common/EditPage.vue
+++ src/view/common/EditPage.vue
@@ -149,7 +149,7 @@
               class="btn btn-sm bg-gradient-primary btn-round mb-0 me-1 mt-2 mt-md-0"
               href="javascript:;"
               @click="edit"
-              >{{ $t('button.edit') }}</a
+              >{{ $t('button.register') }}</a
             >
             <a
               class="btn btn-sm bg-gradient-primary btn-round mb-0 me-1 mt-2 mt-md-0"
